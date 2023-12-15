dbd67207a71aae7ffb456324f904d9a1d39c224d
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-12-15 12:57:34 +0000

    Update scalatest to 3.2.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   lazy val preservicaClient = "uk.gov.nationalarchives" %% "preservica-client-fs2" % "0.0.35"
   lazy val pureConfigCats = "com.github.pureconfig" %% "pureconfig-cats-effect" % pureConfigVersion
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % pureConfigVersion
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
   lazy val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.2.0"
   lazy val upickle = "com.lihaoyi" %% "upickle" % "3.1.3"
   lazy val reactorTest = "io.projectreactor" % "reactor-test" % "3.6.1"
