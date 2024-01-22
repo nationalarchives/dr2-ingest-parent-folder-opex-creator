4d4473a558a4515ec1a72dbfd2a154d930116f79
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2024-01-22 14:29:08 +0000

    DR2-1432 Improve logging
    There is only a couple of lines in here but I've parsed the batch ref
    out of the execution name so we should be able to see the parent opex
    being created.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,7 @@ import sbt._
 object Dependencies {
   private val mockitoScalaVersion = "1.17.30"
   lazy val pureConfigVersion = "0.17.5"
+  private val log4CatsVersion = "2.6.0"
 
   lazy val awsS3Client = "uk.gov.nationalarchives" %% "da-s3-client" % "0.1.35"
   lazy val fs2Core = "co.fs2" %% "fs2-core" % "3.9.4"
@@ -9,6 +10,8 @@ object Dependencies {
   lazy val log4jSlf4j = "org.apache.logging.log4j" % "log4j-slf4j-impl" % logbackVersion
   lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % logbackVersion
   lazy val log4jTemplateJson = "org.apache.logging.log4j" % "log4j-layout-template-json" % logbackVersion
+  lazy val log4CatsCore= "org.typelevel" %% "log4cats-core" % log4CatsVersion
+  lazy val log4CatsSlf4j = "org.typelevel" %% "log4cats-slf4j" % log4CatsVersion
   lazy val lambdaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
   lazy val lambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.11.4"
   lazy val mockitoScala = "org.mockito" %% "mockito-scala" % mockitoScalaVersion
